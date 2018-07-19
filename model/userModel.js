7fafb78d38c4fc4c3998647a4c8781836cc58715
jideajayi11 <jideajayi11@gmail.com>
2018-07-18 22:40:30 -0700

    create dummy data [#159059483]

---
+++ model/userModel.js
@@ -0,0 +1,19 @@
+const userModel = [
+  {
+    id: 1,
+    email: 'segunadams@gmail.com',
+    password: 'qwerty',
+    fullName: 'Segun Adams',
+    reminderTime: '07:00 am',
+    dateAdded: '2018-01-20'
+  },
+  {
+    id: 2,
+    email: 'tundebabs@gmail.com',
+    password: 'asdfgh',
+    fullName: 'Tunde Badmus',
+    reminderTime: '07:00 am',
+    dateAdded: '2018-01-20'
+  }
+];
+export default userModel;
