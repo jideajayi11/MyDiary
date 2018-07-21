49b2ffddd7e32e3932336067a84f4b774dcb8ed1
jideajayi11 <jideajayi11@gmail.com>
2018-07-20 18:47:52 -0700

    test put time api

--- controller/timeSettingController.js
+++ controller/timeSettingController.js
@@ -2,5 +2,41 @@ import userModel from '../model/userModel';
 
 class Time {
 
+    static setTime (req, res) {
+
+        const user = userModel.
+        filter((item) => item.id === parseInt(req.params.id, 10));
+        if (req.body.time !== undefined) {
+    
+          user[0].reminderTime = req.body.time;
+    
+    }
+        const index = userModel.
+        findIndex((item) => item.id === parseInt(req.params.id, 10));
+        if (index >= 0) {
+    
+          userModel.splice(index, 1, {
+            dateAdded: user[0].dateAdded,
+            email: user[0].email,
+            fullName: user[0].fullName,
+            id: user[0].id,
+            password: user[0].password,
+            reminderTime: user[0].reminderTime
+          });
+    
+    return res.status(200).json({
+            message: 'Entry updated',
+            user
+          });
+    
+    }
+    
+    return res.status(404).json({
+          error: 404,
+          message: 'Entry not found'
+        });
+    
+      }
+      
 }
 export default Time;
--- test/timeSetting.test.js
+++ test/timeSetting.test.js
@@ -4,3 +4,25 @@ import server from '../bin/www';
 
 const should = chai.should();
 chai.use(chaiHttp);
+
+
+describe('UPDATE Time', () => {
+
+    it('should Update the reminder time', (done) => {
+  
+      chai.request(server).
+        put('/api/v1/reminder/1').
+        send({
+          reminderTime: '07:00 am'
+        }).
+        end((err, res) => {
+  
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+  
+  });
+  
+  });
+  
+  });
